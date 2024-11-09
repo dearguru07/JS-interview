6ab47d415f9e49edb5e13be7936e5293f68cf086
dearguru07 <pangalaguruyadav101@gmail.com>
2024-11-09 18:44:12 +0530

    js

---
+++ app.js
@@ -0,0 +1,141 @@
+// let container=document.getElementById('container')
+// let input=document.createElement("h3")
+
+// container.innerHTML="<p>Hello too all</p>"
+
+// const element = document.getElementById("example");
+// element.innerHTML = "<b>Hello, <i>World!</i></b> <span style='display:none;'>Hidden Text</span>";
+
+// element.innerText="Hello every one"
+// input.innerHTML="Morning to all....."
+// container.appendChild(input)
+
+// -----------------------innerText-------------------------
+
+// const elementOne = document.getElementById("example2");
+// console.log(elementOne.innerText);
+// elementOne.innerText = "<b>Hello, World!</b>";
+
+// ----------------textContent----------------------
+
+// const element = document.getElementById("example3");
+// console.log(element.textContent); // Logs: "Hello, World! Hidden Text"
+// element.textContent = "<b>Hello, World.........!</b>";
+
+//-------------------- setAttribute-------------------
+
+// const element = document.getElementById("example");
+// element.setAttribute("class", "my-class");
+// element.setAttribute("data-id", "123");
+
+// element.style.margin="20px auto"
+// element.setAttribute("class",'newClass')
+
+// element.removeAttribute("class",'newClass')
+// element.classList=null
+
+// let a = 20;
+// console.log(typeof a);
+// console.log(a);
+// console.log(5);
+
+
+// let a = `254`;
+// console.log(a);
+
+
+// let name = 'Alice';
+// let greeting = 'Hello, ' + name + '!';
+
+// console.log(name)
+// console.log(greeting)
+
+// let name = 'Alice';
+// let greeting = `Hello, ${name}!`; // String interpolation
+// console.log(greeting); // Outputs: Hello, Alice!
+
+// let multilineString = `This is
+// a multiline
+// string!`; // Multiline string without special characters
+// console.log(multilineString)
+
+// let myVariable = 3;
+// let res=!(myVariable === 3);
+// console.log(res)
+
+// let myVariables = 3;
+// let sol=myVariables !== 3;
+// console.log(sol)
+
+// let iceCream = "chocolate";
+// if (iceCream === "chocolate") {
+//   console.log("Yay, I love chocolate ice cream!");
+// } else {
+//   console.log("Awwww, but chocolate is my favorite…");
+// }
+
+// let myVariable = document.querySelector("h1");
+// alert('hello')
+
+
+
+// function multiply(num1, num2) {
+//   let result = num1 * num2;
+//   return result;
+// }
+// let res=multiply(4, 7);
+// console.log(res)
+
+
+
+// let a=[1,25,4,7,8,2,6]
+// let res=a.map((x)=>{
+//     return x+x
+// })
+// console.log(res)
+
+
+// let a=[4,57,8,9,5,2,6,3,1,4,2,3]
+// let res=a.filter((x)=>{
+//     return x<5
+// })
+// console.log(res)
+
+
+// for(var i=0;i<3;i++){
+// setTimeout(function(){
+//     alert(i)
+// },100+i)
+// }
+
+
+
+// (function(){
+//     var a=b=10
+
+// }
+// ())
+// console.log(b)
+
+
+
+// async function get(){
+//     return await Promise.resolve('Hello')
+// }
+// const data=get()
+// console.log(data)
+
+
+// function Afer(){
+//     return new Promise(()=>{
+//         setTimeout(()=>{
+//             <FUNCTION>('resolved')
+//         })
+//     })
+// }
+
+
+// for (var i=0;i<3;i++){
+//     setTimeout(function(){alert(i);},1000+i)
+// }
+
