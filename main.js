97bdc3f2e79271c78f08aa5b8b3408b6d488610c
dearguru07 <pangalaguruyadav101@gmail.com>
2024-11-15 13:03:34 +0530

    js

---
+++ main.js
@@ -0,0 +1,18 @@
+// function Expression-------------
+
+// Solution() --error
+// add() --- error
+// let add=function Solution(){
+//         console.log('Hello to all')
+// }
+// add()
+
+
+function a() {
+let b = 20;
+    c()
+    function c(){
+        console.log(b);
+    }
+}
+a();
