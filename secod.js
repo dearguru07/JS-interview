08f03fd6dfcb6d405bed9b1e27949098e6afc8f8
dearguru07 <pangalaguruyadav101@gmail.com>
2024-11-21 14:51:25 +0530

    k

--- secod.js
+++ secod.js
@@ -9,14 +9,28 @@
 
 // let heading=document.createComment
 
-
 // let a=100
-{
-    let a = 200
-    console.log(a)
-    {
-        let a =300
-        console.log(a)
-    }
+// {
+//     let a = 200
+//     console.log(a)
+//     {
+//         let a =300
+//         console.log(a)
+//     }
+// }
+// console.log(a)
+
+function Click() {
+  let count = document.getElementById("count");
+  let name = document.getElementById("name").value;
+  let passWord = document.getElementById("password").value;
+  let alert = document.createElement("p");
+  let para = document.createElement("p");
+  if (name || passWord == "") {
+    alert.innerText = "Pls fill the all feilds";
+    count.appendChild(alert);
+  } else {
+    para.innerText = "Applied Successfully";
+    count.appendChild(para);
+  }
 }
-console.log(a)
