88e3954d6b00319ce4f961b70c5450b834424870
dearguru07 <pangalaguruyadav101@gmail.com>
2024-11-21 11:12:04 +0530

    j

--- secod.js
+++ secod.js
@@ -1,8 +1,22 @@
-function a(){
-    c()
-    function c(){
-        console.log(x)
+// function a(){
+//     c()
+//     function c(){
+//         console.log(x)
+//     }
+// }
+// let x=4
+// a()
+
+// let heading=document.createComment
+
+
+// let a=100
+{
+    let a = 200
+    console.log(a)
+    {
+        let a =300
+        console.log(a)
     }
 }
-let x=4
-a()
+console.log(a)
